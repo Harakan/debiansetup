e5cff8a1b35ff16c40f67d73d18413f391fbe0ce
root <root@localhost.localdomain>
2017-09-30 11:56:25 -0500

    touched up scripts

--- helper_scripts/add_user.sh
+++ helper_scripts/add_user.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 USER=willbox
+
+if [ $# -eq 1 ]
+    then
+        USER=$1
+fi
+
+echo "Adding user $USER"
+echo
+
 useradd -m $USER
 passwd $USER
 #usermod -a -G sudo $USER #UNCOMMENT FOR ROOT
--- helper_scripts/convert.sh
+++ helper_scripts/convert.sh
@@ -2,7 +2,7 @@
 
 #"for i in *mkv; do echo $i && echo "$i"'.mp4'; done
 
-#A="Yuri!!! on Ice - 01 (1280x720 HEVC2 AAC).mkv"
+#A="video.mkv"
 #ffmpeg -i "$A" -movflags +faststart -acodec copy -vcodec copy "$A"'.mp4' "$A"'.srt'
 #ffmpeg -i "$A" -movflags +faststart -acodec copy -vcodec copy "$A"'.mp4'
 #ffmpeg -i "$A" -movflags +faststart -map 0:2 "$A"'2.mp4'
